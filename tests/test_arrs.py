60709f02d1e8f0b05b733b5fbd4ca388edefa806
Nikolay <n.avramenko87@gmail.com>
2023-02-19 00:03:06 +0300

    add texture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,9 +12,12 @@ def test_get_default():
     assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 
+@pytest.fixture
+def datas():
+    return [1, 2, 3, 4]
 
-def test_my_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+def test_my_slice(datas):
+    assert arrs.my_slice(datas, 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
 
 
@@ -22,8 +25,8 @@ def test_my_slice_zero_list():
     assert arrs.my_slice([], 1, "test") == []
 
 
-def test_my_slice_otr_1():
-    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
+def test_my_slice_otr_1(datas):
+    assert arrs.my_slice(datas, -2) == [3, 4]
 
-def test_my_slice_otr_2():
-    assert arrs.my_slice([1, 2, 3, 4], -10) == [1, 2, 3, 4]
+def test_my_slice_otr_2(datas):
+    assert arrs.my_slice(datas, -10) == [1, 2, 3, 4]
